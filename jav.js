fd8bbee7dfcd1894a0ade7351210a59fd0216e9d
azizdriss <41290215+azizdriss@users.noreply.github.com>
2019-06-25 10:55:27 +0200

    Add files via upload

---
+++ jav.js
@@ -0,0 +1,32 @@
+let pets = [
+    { name: "Max", type: "dog", bornOn: 2018 },
+    { name: "Angel", type: "cat", bornOn: 2015 },
+    { name: "Jasper", type: "dog", bornOn: 2016 }
+  ];
+  
+  const getAge= (pet) => {
+    return new Date().getFullYear() - pet.bornOn;
+  }
+  
+  let petsWithAge = [];
+  pets.forEach(pet => {
+    pet.age = getAge(pet);
+    petsWithAge.push(pet);
+  });
+  console.log(petsWithAge);
+  
+let dogs = [];
+pets.forEach(pet => {
+    if (pet.type === "dog") {
+        dogs.push(pet);
+      }
+})
+console.log(dogs);
+  
+  let jasper;
+  pets.find(pet => {
+    if (pet.name === "Jasper") {
+      jasper = pet;
+    }
+  })
+  console.log(`Jasper is ${jasper.age} years old`);
